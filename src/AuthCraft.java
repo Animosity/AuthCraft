e14c985ccba057c5c7d35f6ab457a5f52b9a18bf
Animosity <nullcline@gmail.com>
2010-12-19 22:47:15 -0700

    'first'

---
+++ src/AuthCraft.java
@@ -0,0 +1,419 @@
+
+import java.io.File;
+import java.io.FileWriter;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+public class AuthCraft extends Plugin {
+
+    private static String NAME = "AuthCraft";
+    private static int MAJOR = 1;
+    private static int MINOR = 0;
+    private static int REVISION = 0;
+
+    private PropertiesFile properties;
+    /**
+     * The file where auths are saved to
+     */
+    private static String AUTH_FILE = "auths.db";
+    /**
+     * The users that have authenticated
+     */
+    public final List<String> authenticated = new ArrayList<String>();
+    /**
+     * The loaded auth values
+     */
+    public Map<String, String> authTable = new HashMap<String, String>();
+    /**
+     * internal use, temporary for players
+     */
+    public final Map<String, InventoryBackup> playerCache = new HashMap<String, InventoryBackup>();
+    /**
+     * When the last "You need to be identified to do that!" message was sent
+     */
+    public long lastAlert = 0;
+    /**
+     * The mode of the server
+     */
+    public boolean onlineMode;
+    /**
+     * If registrations are accepted while in offline mode
+     */
+    public boolean registerInOfflineMode;
+    private AuthCraftListener listener = null;
+    public boolean onlyAllowedUsersCanRegister;
+
+    public void initialize() {
+        //log("Registering listeners");
+
+        listener = new AuthCraftListener(this);
+
+        register(PluginLoader.Hook.BLOCK_CREATED);
+        register(PluginLoader.Hook.BLOCK_DESTROYED);
+        register(PluginLoader.Hook.CHAT);
+        register(PluginLoader.Hook.COMMAND);
+        register(PluginLoader.Hook.DISCONNECT);
+        register(PluginLoader.Hook.LOGIN);
+        register(PluginLoader.Hook.PLAYER_MOVE);
+        register(PluginLoader.Hook.SERVERCOMMAND);
+        register(PluginLoader.Hook.INVENTORY_CHANGE);
+        register(PluginLoader.Hook.CRAFTINVENTORY_CHANGE);
+        register(PluginLoader.Hook.EQUIPMENT_CHANGE);
+        register(PluginLoader.Hook.ITEM_DROP);
+    }
+
+    /**
+     * Register a hook
+     *
+     * @param hook the hook to register
+     * @priority the priority to use
+     */
+    private void register(PluginLoader.Hook hook, PluginListener.Priority priority) {
+        //log("MineListener -> " + hook.toString());
+
+        etc.getLoader().addListener(hook, listener, this, priority);
+    }
+
+    /**
+     * Register a hook with default priority
+     *
+     * @param hook the hook to register
+     */
+    private void register(PluginLoader.Hook hook) {
+        register(hook, PluginListener.Priority.MEDIUM);
+    }
+
+    /**
+     * Internal use - log a player in
+     *
+     * @param player
+     *            the player to login
+     */
+    public boolean _login(Player player) {
+        if (etc.getDataSource().doesPlayerExist(player.getName())) {
+            player.setAdmin(etc.getDataSource().getPlayer(player.getName()).isAdmin());
+            player.setCanModifyWorld(etc.getDataSource().getPlayer(player.getName()).canBuild());
+            player.setCommands(etc.getDataSource().getPlayer(player.getName()).getCommands());
+            player.setGroups(etc.getDataSource().getPlayer(player.getName()).getGroups());
+            player.setIgnoreRestrictions(etc.getDataSource().getPlayer(player.getName()).canIgnoreRestrictions());
+            //IF THE REGISTERED FEW TIME AGO, HE MIGHT NOT HAVE A BACKUP
+            if (playerCache.containsKey(player.getName().toLowerCase())) {
+                playerCache.get(player.getName().toLowerCase()).restoreBackup();
+                playerCache.remove(player.getName().toLowerCase());
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Add an auth to the valid auths table
+     *
+     * @param username
+     * @param password
+     */
+    public void addAuth(String username, String password) {
+        if (authTable.containsKey(username.toLowerCase())) {
+            authTable.remove(username);
+        }
+
+        authTable.put(username.toLowerCase(), password);
+        saveAuthEntries();
+    }
+
+    /**
+     * Validate a player's auth
+     *
+     * @param player
+     *            the Player to check
+     * @return true if the player is not authenticated. false if the player is
+     *         authenticated or not registered
+     */
+    public boolean checkAuth(Player player) {
+        /*if (!etc.getDataSource().doesPlayerExist(player.getName())) {
+            return true;
+        }*/
+    	
+        if (!authenticated.contains(player.getName().toLowerCase())) {
+            if (authTable.containsKey(player.getName().toLowerCase())) {
+                if (lastAlert == 0
+                        || System.currentTimeMillis() - lastAlert > 1000) {
+                    player.sendMessage(Colors.Rose
+                            + "Please identify yourself with /login <password>");
+                    lastAlert = System.currentTimeMillis();
+                }
+
+                return true;
+            } else if (onlineMode || registerInOfflineMode) {
+                if (lastAlert == 0
+                        || System.currentTimeMillis() - lastAlert > 1000) {
+                    player.sendMessage(Colors.Rose
+                            + "Please register yourself with /register <password>");
+                    lastAlert = System.currentTimeMillis();
+                }
+
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Copy key vars from one Player instance to another
+     *
+     * @param player1
+     *            the player to copy from
+     * @param player2
+     *            the player to copy to
+     * @return the copied player
+     */
+    private Player copyAccountData(Player player, Player player_) {
+        player_.setAdmin(player.isAdmin());
+        player_.setCanModifyWorld(player.canModifyWorld());
+        player_.setCommands(player.getCommands());
+        player_.setGroups(player.getGroups());
+        player_.setIgnoreRestrictions(player.canIgnoreRestrictions());
+
+        return player_;
+    }
+
+    /**
+     * When the plugin is disabled the plugin will cease to function
+     */
+    @Override
+    public void disable() {
+        log(this.NAME + " - Server-side authentication disabled!");
+        saveAuthEntries();
+    }
+
+    /**
+     * When the plugin is enabled (including when the server is just started)
+     */
+    @Override
+    public void enable() {
+    	
+        setName(NAME);
+        log(this.NAME + " - Server-side authentication enabled.");
+
+        properties = new PropertiesFile("server.properties");
+        onlineMode = properties.getBoolean("online-mode", true);
+        onlyAllowedUsersCanRegister = properties.getBoolean("require-register-command", false);
+        registerInOfflineMode = properties.getBoolean("register-offline", false);
+
+        loadAuthEntries();
+        saveAuthEntries(); // create the file initially if it does not exist
+
+        log("Loaded " + authTable.size() + " registrations");
+    }
+
+    /**
+     * The encryption implementation to store passwords [as md5 (default)]
+     *
+     * @param string
+     *            the string to encrypt
+     * @default the encrypted string
+     */
+    public String encrypt(String string) {
+        try {
+            final MessageDigest m = MessageDigest.getInstance("MD5");
+            final byte[] bytes = string.getBytes();
+            m.update(bytes, 0, bytes.length);
+            final BigInteger i = new BigInteger(1, m.digest());
+
+            return String.format("%1$032X", i).toLowerCase();
+        } catch (final Exception e) {
+        }
+
+        return "";
+    }
+
+    /**
+     * Load the saved auths (if there are any!)
+     */
+    private void loadAuthEntries() {
+        final File file = new File(AUTH_FILE);
+
+        if (!file.exists()) {
+            return;
+        }
+
+        Scanner reader = null;
+        int lineCount = 0;
+        try {
+            reader = new Scanner(file);
+            while (reader.hasNextLine()) {
+                lineCount++;
+                reader.nextLine();
+            }
+        } catch (final Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                reader.close();
+            }
+        }
+        if (lineCount > 150) {
+            authTable = new HashMap<String, String>(lineCount + ((int) (lineCount * 0.40)));
+            
+        }
+
+        try {
+            reader = new Scanner(file);
+            while (reader.hasNextLine()) {
+                final String line = reader.nextLine();
+
+                if (!line.contains(":")) {
+                    /*
+                     * Invalid!
+                     */
+                    continue;
+                }
+
+                final String[] in = line.split(":");
+
+                if (in.length != 2) {
+                    continue;
+                }
+
+                final String username = in[0].toLowerCase();
+                final String password = in[1];
+
+                addAuth(username, password);
+            }
+        } catch (final Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                reader.close();
+            }
+        }
+    }
+
+    /**
+     * Log a message
+     *
+     * @param str
+     *            the string to log
+     */
+    public void log(String str) {
+        System.out.println("[" + getName() + "] [v" + MAJOR + "." + MINOR + "."
+                + REVISION + "] " + str);
+    }
+
+    /**
+     * Get all of the inherited groups for a player
+     *
+     * @param player the player to get the inherited groups for
+     * @return the array of inherited groups
+     */
+    public ArrayList<String> getInherited(String group_) {
+        ArrayList<String> list = new ArrayList<String>();
+
+        Group group = etc.getDataSource().getGroup(group_);
+
+        if (group == null) {
+            return null;
+        }
+        if (group.Name.equals(etc.getInstance().getDefaultGroup().Name)) {
+            return list;
+        }
+
+        for (String g : group.InheritedGroups) {
+            if (g.length() == 0) {
+                continue;
+            }
+
+            list.add(g);
+
+            for (String _g : getInherited(g)) {
+                list.add(_g);
+            }
+        }
+
+        return list;
+    }
+
+    /**
+     * Save the auths
+     */
+    public void saveAuthEntries() {
+        final File file = new File(AUTH_FILE);
+
+        if (file.exists()) {
+            file.delete();
+        }
+
+        FileWriter writer = null;
+
+        try {
+            file.createNewFile();
+
+            writer = new FileWriter(file);
+
+            for (final String username : authTable.keySet()) {
+                final String password = authTable.get(username);
+
+                writer.write(username + ":" + password + "\r\n");
+                writer.flush();
+            }
+
+            writer.close();
+        } catch (final Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    /**
+     * Transform a string into one char
+     *
+     * @param str
+     *            The string to transform
+     * @param chr
+     *            The char to transform all chars to (ie '*')
+     * @return the transformed string
+     */
+    public String transform(String str, char chr) {
+        final char[] charArray = str.toCharArray();
+
+        for (int i = 0; i < charArray.length; i++) {
+            charArray[i] = '*';
+        }
+
+        return new String(charArray);
+    }
+
+    /**
+     * Update the player cache
+     *
+     * @param player
+     *            the player to update
+     */
+    public void updatePlayerCache(Player player) {
+        player.setCanModifyWorld(false); // lock them down!!
+        player.setIgnoreRestrictions(false);
+        player.setAdmin(false);
+        if (canRegister(player)) {
+            player.setCommands(new String[]{"register", "login"});
+        } else {
+            player.setCommands(new String[0]);
+        }
+        player.setGroups(new String[0]);
+        if (etc.getDataSource().doesPlayerExist(player.getName())) {
+            Player p = etc.getInstance().getDataSource().getPlayer(player.getName());
+            if (p.canBuild()) {
+                player.setCanModifyWorld(true);
+                //playerCache.put(player.getName().toLowerCase(), new InventoryBackup(player));
+            }
+        }
+
+    }
+    public boolean canRegister(Player player){
+        return !onlyAllowedUsersCanRegister || player.canUseCommand("/register");
+    }
+}
---
+++ src/AuthCraftListener.java
@@ -0,0 +1,293 @@
+import java.util.ArrayList;
+
+public class AuthCraftListener extends PluginListener {
+	boolean _DEBUG = false;
+
+	private AuthCraft parent = null;
+
+	public AuthCraftListener(AuthCraft parent) {
+		this.parent = parent;
+	}
+
+	public boolean onBlockCreate(Player player, Block blockPlaced, Block blockClicked, int itemInHand) {
+		/*if (_DEBUG) {
+			System.out.println(Thread.currentThread().getStackTrace()[1].getMethodName());
+			System.out.println(Boolean.toString(parent.checkAuth(player)));
+		}*/
+		return parent.checkAuth(player);
+	}
+
+	public boolean onBlockDestroy(Player player, Block block) {
+		return parent.checkAuth(player);
+	}
+
+	public boolean onChat(Player player, String message) {
+		return parent.checkAuth(player);
+	}
+
+	public boolean onInventoryChange(Player player) {
+		return parent.checkAuth(player);
+	}
+
+	public boolean onCraftInventoryChange(Player player) {
+		return parent.checkAuth(player);
+	}
+
+	public boolean onEquipmentChange(Player player) {
+		return parent.checkAuth(player);
+	}
+
+	public boolean onItemDrop(Player player, Item item) {
+		return parent.checkAuth(player);
+	}
+
+	/**
+	 * Login/register users!
+	 * 
+	 * sidenote: why a string array???? wat.
+	 */
+	@Override
+	public boolean onCommand(Player player, String[] split) {
+		if (split.length == 0) {
+			return false;
+		}
+
+		final String command = split[0].substring(1);
+
+		if (command.equalsIgnoreCase("login")) {
+			if (split.length != 2) {
+				player.sendMessage("Usage: /login <pass>");
+				return true;
+			}
+
+			final String username = player.getName().toLowerCase();
+			final String password = split[1];
+
+			/*
+			 * They are registered! If they aren't - don't do anything
+			 */
+			if (parent.authTable.containsKey(username) && !parent.authenticated.contains(username)) {
+				final String realPassword = parent.authTable.get(username);
+
+				if (!realPassword.equals(parent.encrypt(password))) {
+					parent.log("Warning: " + player.getName().toLowerCase() + " gave the wrong password !! IP: "
+							+ player.getIP());
+					if (parent.playerCache.containsKey(player.getName().toLowerCase())) {
+						parent.playerCache.get(player.getName().toLowerCase()).restoreBackup();
+						parent.playerCache.remove(player.getName().toLowerCase());
+					}
+					player.kick("Invalid password."); // be forceful! leave no
+					// prisoners!
+				} else {
+					player.sendMessage(Colors.Green + "Password accepted. Welcome.");
+					parent.authenticated.add(player.getName().toLowerCase());
+
+					return parent._login(player);
+				}
+			}
+
+			return true;
+		} else if (command.equalsIgnoreCase("register")) {
+			if (!parent.onlineMode && !parent.registerInOfflineMode) {
+				return true;
+			}
+
+			if (split.length != 2) {
+				player.sendMessage("Usage: /register <pass>");
+				return true;
+			}
+
+			final String username = player.getName().toLowerCase();
+			final String password = split[1];
+
+			if (parent.authTable.containsKey(username)) {
+				player.sendMessage(Colors.Red + "Your nick is already registered!");
+			} else {
+				parent.addAuth(username, parent.encrypt(password));
+
+				player.sendMessage(Colors.Green + "Registered using the password: " + Colors.LightBlue
+						+ parent.transform(password, '*'));
+				parent.authenticated.add(player.getName().toLowerCase());
+
+				parent._login(player);
+
+				return true;
+			}
+
+			return true;
+		} else if (command.equalsIgnoreCase("pass")) {
+			if (!parent.authenticated.contains(player.getName().toLowerCase())) {
+				player.sendMessage(Colors.Rose + "You must be authenticated to change your password");
+				return true;
+			}
+
+			if (split.length != 3) {
+				player.sendMessage("Usage: /pass <oldpass> <newpass>");
+				return true;
+			}
+
+			final String oldPass = split[1];
+			final String newPass = split[2];
+
+			if (!parent.authTable.get(player.getName().toLowerCase()).equals(parent.encrypt(oldPass))) {
+				player.sendMessage(Colors.Rose + "Invalid password.");
+				return true;
+			}
+
+			parent.authTable.remove(player.getName().toLowerCase());
+			parent.addAuth(player.getName(), parent.encrypt(newPass));
+			parent.authenticated.remove(player.getName().toLowerCase());
+
+			player.sendMessage(Colors.Green + "Changed your password to: " + Colors.LightBlue
+					+ parent.transform(newPass, '*'));
+			player.sendMessage(Colors.Rose + "Please reauthenticate");
+			parent.updatePlayerCache(player);
+
+			return true;
+		} else if (command.equalsIgnoreCase("reset")) {
+			// player.sendMessage("can't use");
+			if (!player.canUseCommand("/" + command)) {
+				return false;
+			}
+			// player.sendMessage("can use");
+			for (int i = 1; i < split.length; i++) {
+				// player.sendMessage("inside for");
+				final String playerName = split[i].toLowerCase();
+				Player _player = etc.getDataSource().getPlayer(playerName);
+
+				if (_player == null) {
+					player.sendMessage(Colors.Rose + "Player " + Colors.LightBlue + playerName + Colors.Rose
+							+ " not found.");
+					continue;
+				}
+				Group mainGroup;
+				if (_player.getGroups().length > 0) {
+					mainGroup = etc.getDataSource().getGroup(_player.getGroups()[0]);
+				} else {
+					mainGroup = etc.getInstance().getDefaultGroup();
+				}
+
+				if (mainGroup == null) {
+					player.sendMessage(Colors.Rose + "Player " + Colors.LightBlue + playerName + Colors.Rose
+							+ " has invalid group on Data.");
+					continue;
+				}
+
+				ArrayList<String> inherited = parent.getInherited(mainGroup.Name);
+
+				if (inherited == null) {
+					player.sendMessage(Colors.Rose + "Player " + Colors.LightBlue + playerName + Colors.Rose
+							+ " not found.");
+					continue;
+				}
+
+				boolean match = false;
+
+				for (String group : player.getGroups()) {
+					// player.sendMessage("inside for, of checking groups");
+					if (group.equals("admins")) {
+						match = false;
+						break;
+					}
+
+					if (mainGroup.Name.equals(group)) {
+						match = true;
+						break;
+					}
+
+					if (inherited.contains(group)) {
+						match = true;
+						break;
+					}
+				}
+
+				if (match) {
+					player.sendMessage(Colors.Rose + "You do not have enough rights to reset " + Colors.LightBlue
+							+ playerName);
+					continue;
+				}
+
+				if (parent.authTable.containsKey(playerName)) {
+					// player.sendMessage("removing authentication");
+					parent.authTable.remove(playerName);
+
+					if (parent.authenticated.contains(playerName)) {
+						parent.authenticated.remove(playerName);
+					}
+
+					player.sendMessage(Colors.Green + "Player registration " + Colors.LightBlue + playerName
+							+ Colors.Green + " reset");
+				} else {
+					player.sendMessage(Colors.Rose + "Player " + Colors.LightBlue + playerName + Colors.Rose
+							+ " is not registered.");
+				}
+			}
+			// player.sendMessage("Outside for!");
+			parent.saveAuthEntries();
+
+			return true;
+		}
+
+		if (!parent.authenticated.contains(player.getName().toLowerCase())) {
+			return true;
+		}
+
+		return false;
+
+	}
+
+	public void onDisconnect(Player player) {
+		if (parent.authenticated.contains(player.getName().toLowerCase())) {
+			parent.authenticated.remove(player.getName().toLowerCase());
+		}
+
+		else {
+			if (parent.playerCache.containsKey(player.getName().toLowerCase())) {
+				parent.playerCache.remove(player.getName().toLowerCase());
+			}
+		}
+	}
+
+	/**
+	 * Log the player in ! :)
+	 */
+
+	public void onLogin(Player player) {
+		if (parent.authTable.containsKey(player.getName().toLowerCase())) {
+			player.sendMessage(Colors.Rose + "Please identify yourself with /login <password>");
+		} else if (parent.onlineMode || parent.registerInOfflineMode) {
+			if (parent.canRegister(player)) {
+				player.sendMessage(Colors.Rose + "You need to register your account before playing.");
+				player.sendMessage(Colors.Yellow + "Do NOT use your forum");
+				player.sendMessage(Colors.Yellow + "or actual Minecraft account password!");
+				player.sendMessage(Colors.Rose + "To register, type /register <password>");
+			}
+		}
+
+		parent.updatePlayerCache(player);
+	}
+
+	@Override
+	public void onPlayerMove(Player player, Location from, Location to) {
+		final boolean result = parent.checkAuth(player);
+
+		if (result) {
+			player.teleportTo(from);
+		}
+	}
+
+	/**
+	 * Called before the console command is parsed. Return true if you don't
+	 * want the server command to be parsed by the server.
+	 * 
+	 * @param split
+	 * @return false if you want the command to be parsed.
+	 */
+	@Override
+	public boolean onConsoleCommand(String[] split) {
+		if (split[0].contains("save-all") || split[0].contains("stop") || split[0].contains("reload")) {
+
+		}
+		return false;
+	}
+}
